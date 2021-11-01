1789bdd928d3e1c6526f3c7533cbf73e893a8f24
renan628 <renanoliveira628@gmail.com>
2021-10-31 23:59:46 -0300

    Modo de paginação cards
    Alterando modo de paginação de link para número na listagem de cards

--- myapi/api/pagination.py
+++ myapi/api/pagination.py
@@ -5,3 +5,17 @@ class CardPagination(pagination.LimitOffsetPagination):
     limit_query_param = 'limit'
     offset_query_param = 'skip'
     max_limit = 50
+
+    def get_next_link(self):
+        if self.offset + self.limit >= self.count:
+            return None
+
+        offset = self.offset + self.limit
+        return offset
+
+    def get_previous_link(self):
+        if self.offset <= 0:
+            return None
+
+        offset = self.offset - self.limit
+        return offset
